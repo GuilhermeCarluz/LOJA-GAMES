c08c021c9a0029d618d5e830280e4cbc5c8838c5
mayaralm <mmayaraleitedemelo@gmail.com>
2021-11-05 12:04:26 -0300

    @mayaralm[Task: initial commit]

---
+++ src/main/java/com/DevGames/lojaTech/LojaTechApplication.java
@@ -0,0 +1,13 @@
+package com.DevGames.lojaTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaTechApplication.class, args);
+	}
+
+}
